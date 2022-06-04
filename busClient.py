5428862adf840bb05e1db131ce975a5f51d1c199
YukkuriC <yukkuri@pku.edu.cn>
2022-06-04 12:12:54 +0800

    ver1.2 新增圆明园班车预约

--- busClient.py
+++ busClient.py
@@ -13,7 +13,7 @@ class BusClient():
                 # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36"
             }
 
-    def __init__(self, appId = 40,headers = __headers):
+    def __init__(self, appId = 40, headers = __headers):
         self.__session = requests.session()
         self.appId = appId
 
